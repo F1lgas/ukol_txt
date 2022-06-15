92253d8e397a86d3b4d375f16369622b92ef8e03
Filip Iran <iranf.03@spst.cz>
2022-06-15 16:52:14 +0200

    Přidejte soubory projektu.

---
+++ ukol_txt/Form1.cs
@@ -0,0 +1,17 @@
+namespace ukol_txt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog cisla = new OpenFileDialog();
+
+
+        }
+    }
+}
