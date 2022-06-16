f2374dd8173edd844d7a6e6310cafaeb37128610
Filip Iran <iranf.03@spst.cz>
2022-06-16 18:17:54 +0200

    v1.0

--- ukol_txt/Form1.cs
+++ ukol_txt/Form1.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace ukol_txt
 {
     public partial class Form1 : Form
@@ -9,9 +11,37 @@ namespace ukol_txt
 
         private void button1_Click(object sender, EventArgs e)
         {
-            OpenFileDialog cisla = new OpenFileDialog();
+           /* int vysledek = 0;
 
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                string radek = listBox1.Items[i].ToString();
 
+                int cislo = Convert.ToInt32(radek.Split(" "));
+
+                vysledek += cislo;
+            }*/
+        }
+
+        private void otevøítToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string jmenosouboru = string.Empty;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.Clear();
+                jmenosouboru = openFileDialog1.FileName;
+                StreamReader cteni = new StreamReader(jmenosouboru, Encoding.UTF8);
+
+                while (!cteni.EndOfStream)
+                {
+                    string veta = cteni.ReadLine();
+
+                    listBox1.Items.Add(veta);
+                }
+
+                cteni.Close();
+            }
         }
     }
 }
