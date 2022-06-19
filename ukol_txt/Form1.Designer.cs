73bd3fdcb96807f5002361037a5db716412fb998
Filip Iran <iranf.03@spst.cz>
2022-06-19 12:38:40 +0200

    v1.1

--- ukol_txt/Form1.Designer.cs
+++ ukol_txt/Form1.Designer.cs
@@ -32,15 +32,11 @@
             this.button1 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.souborToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.otevřítToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(138, 27);
+            this.button1.Location = new System.Drawing.Point(138, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 28);
             this.button1.TabIndex = 0;
@@ -56,54 +52,24 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 15;
-            this.listBox1.Location = new System.Drawing.Point(12, 27);
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(120, 124);
             this.listBox1.TabIndex = 1;
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.souborToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(225, 24);
-            this.menuStrip1.TabIndex = 2;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // souborToolStripMenuItem
-            // 
-            this.souborToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.otevřítToolStripMenuItem});
-            this.souborToolStripMenuItem.Name = "souborToolStripMenuItem";
-            this.souborToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
-            this.souborToolStripMenuItem.Text = "Soubor";
-            // 
-            // otevřítToolStripMenuItem
-            // 
-            this.otevřítToolStripMenuItem.Name = "otevřítToolStripMenuItem";
-            this.otevřítToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
-            this.otevřítToolStripMenuItem.Text = "Otevřít";
-            this.otevřítToolStripMenuItem.Click += new System.EventHandler(this.otevřítToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(225, 164);
+            this.ClientSize = new System.Drawing.Size(225, 147);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Úkol textové soubory";
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -112,8 +78,5 @@
         private Button button1;
         private OpenFileDialog openFileDialog1;
         private ListBox listBox1;
-        private MenuStrip menuStrip1;
-        private ToolStripMenuItem souborToolStripMenuItem;
-        private ToolStripMenuItem otevřítToolStripMenuItem;
     }
 }
--- ukol_txt/Form1.cs
+++ ukol_txt/Form1.cs
@@ -11,36 +11,46 @@ namespace ukol_txt
 
         private void button1_Click(object sender, EventArgs e)
         {
-           /* int vysledek = 0;
-
-            for (int i = 0; i < listBox1.Items.Count; i++)
-            {
-                string radek = listBox1.Items[i].ToString();
-
-                int cislo = Convert.ToInt32(radek.Split(" "));
-
-                vysledek += cislo;
-            }*/
-        }
-
-        private void otevøítToolStripMenuItem_Click(object sender, EventArgs e)
-        {
+            listBox1.Items.Clear();
             string jmenosouboru = string.Empty;
+            int celkem = 0;
 
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                listBox1.Items.Clear();
+                int vysledek = 0;
+                int y = 1;
                 jmenosouboru = openFileDialog1.FileName;
                 StreamReader cteni = new StreamReader(jmenosouboru, Encoding.UTF8);
-
+                
                 while (!cteni.EndOfStream)
                 {
                     string veta = cteni.ReadLine();
 
                     listBox1.Items.Add(veta);
+
+                    char[] mezera = { ' ', '\t' };
+                    string[] cisla = veta.Split(mezera);
+
+                    for (int i = 0; i < cisla.Count(); i++)
+                    {
+                        int cislo = Convert.ToInt32(cisla[i]);
+
+                        vysledek += cislo;
+                        celkem += cislo;
+                    }
+                    MessageBox.Show("Souèet èísel v " + y + ". øádku je " + vysledek);
+                    vysledek = 0;
+                    y++;
                 }
 
                 cteni.Close();
+
+                MessageBox.Show("Celkový souèet všech èísel je " + celkem);
+
+                StreamWriter zapis = new StreamWriter(openFileDialog1.FileName, true);
+
+                zapis.WriteLine(Environment.NewLine + "Celkový souèet všech èísel je " + celkem);
+                zapis.Close();
             }
         }
     }
